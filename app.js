25405dda6dc3b16465f17234a71caf57cdf1515d
Akhil Reddy <akhilreddy18@outlook.com>
2018-11-03 18:42:47 +0100

    added database

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express"),
           app = express(),
    bodyParser = require("body-parser"),
-         port = 3000;
+         port = 3000,
+     mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+
+//SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+})
+
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//                 {
+
+//                  name: "Mountain Goat",
+//                  image: "https://farm9.staticflickr.com/8041/7930230882_0bb80ca452.jpg"
+
+//                 }, function(err, campground){
+//                     if(err){
+//                         console.log(err);
+//                     } else {
+//                         console.log("Newly created campground");
+//                         console.log(campground);
+//                     }
+//                 });
+
 
 var campgrounds = [
         {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg"},
@@ -23,15 +49,30 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            coonsole.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});   
+        }
+    })
 });
 
 app.post("/campgrounds", function(req,res){
-    var name = req.body.campgroundName;
-    var image = req.body.campgroundImage;
-    var newCampgroound= {name: name, image: image}
-    campgrounds.push(newCampgroound);
-    res.redirect("/campgrounds");
+
+    var newCampground = 
+    {
+        name: req.body.campgroundName, 
+        image: req.body.campgroundImage
+    }
+
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    })    
 });
 
 app.get("/campgrounds/new", function(req,res){
